0fdb04a425e11a07cdf2c113d640e5868dfd7f12
Alex K. Fong <alexkfong@yahoo.com>
2015-10-07 04:07:23 +0800

    Adds commenting to code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 $(document).ready( function() {
+	
+	// isReady captures whether the mouse is hovering on Ryu
 	var isReady = false;
+	
 	$('.ryu').mouseenter(function() {
     	$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		
+		// Mouse is hovering, make isReady true
 		isReady = true;
   })
 	.mouseleave(function() {
    		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+
+		// Mouse is no longer hovering, make isReady false
 		isReady = false;
   })
 	.mousedown(function() {
@@ -30,6 +37,8 @@ $(document).ready( function() {
 
   	$(document).keydown(function(event) {
   		if (event.which == 88) {
+  			
+  			//if statement chooses which Ryu div to hide based on isReady
   			if(isReady) {
 		  		$('.ryu-ready').hide();
 		  	}
@@ -42,6 +51,8 @@ $(document).ready( function() {
   	.keyup(function(event) {
   		if(event.which == 88) {
   			$('.ryu-cool').hide();
+  			
+  			//if statement chooses which Ryu div to reveal based on isReady
   			if(isReady) {
   				$('.ryu-ready').show();
   			}
