beba96eb463acc10ae169fb7d79fd900630db0a3
Alex K. Fong <alexkfong@yahoo.com>
2015-10-07 04:04:17 +0800

    Implements the cool pose

---
+++ js/app.js
@@ -0,0 +1,59 @@
+$(document).ready( function() {
+	var isReady = false;
+	$('.ryu').mouseenter(function() {
+    	$('.ryu-still').hide();
+		$('.ryu-ready').show();
+		isReady = true;
+  })
+	.mouseleave(function() {
+   		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		isReady = false;
+  })
+	.mousedown(function() {
+    	playHadouken();
+    	$('.ryu-ready').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').finish().show().animate(
+ 			{'left': '1020px'}, 
+ 			500,
+  			function() {
+    			$(this).hide();
+    			$(this).css('left', '520px');
+  			}
+		);
+  })
+  	.mouseup(function() {
+  		$('.ryu-throwing').hide();
+  		$('.ryu-ready').show();
+  });
+
+  	$(document).keydown(function(event) {
+  		if (event.which == 88) {
+  			if(isReady) {
+		  		$('.ryu-ready').hide();
+		  	}
+		  	else {
+		  		$('.ryu-still').hide();
+		  	}
+	  		$('.ryu-cool').show();
+	  	}
+  	})
+  	.keyup(function(event) {
+  		if(event.which == 88) {
+  			$('.ryu-cool').hide();
+  			if(isReady) {
+  				$('.ryu-ready').show();
+  			}
+  			else {
+  				$('.ryu-still').show();
+  			}
+  		}
+  	});
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
